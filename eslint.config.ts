f5b047ae684aebff6a38d901e542d58e11e7ef2b
Xinyao <hi@xinyao.me>
2025-03-14 12:04:47 +0800

    fix: add file copy on success in tsdown config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,3 +1,5 @@
 import { defineConfig } from './eslint/index'
 
-export default defineConfig()
+export default defineConfig({
+  ignores: ['dist'],
+})
--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'tsdown'
+import path from 'node:path'
+import fs from 'fs-extra'
 
 export default defineConfig([
   {
+    onSuccess: () => {
+      const worker = path.join(__dirname, 'eslint/configs/prettier/worker.cjs')
+      fs.copyFileSync(worker, 'dist/eslint/worker.cjs')
+    },
     entry: ['eslint/index.ts'],
     outDir: 'dist/eslint',
     target: 'node18.18',
