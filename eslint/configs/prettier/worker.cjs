e84a0eb17a10a1f1c3c0c30e0a04a88f077e72a9
Xinyao <hi@xinyao.me>
2025-04-30 13:16:35 +0800

    fix(eslint): simplify Prettier formatting by removing config resolution

--- eslint/configs/prettier/worker.cjs
+++ eslint/configs/prettier/worker.cjs
@@ -6,7 +6,5 @@ let prettier
 
 runAsWorker(async (code, options) => {
   if (!prettier) prettier = await import('prettier')
-  const prettierRcOptions = await prettier.resolveConfig(options.onDiskFilepath, { editorconfig: true })
-
-  return prettier.format(code, { ...prettierRcOptions, ...options })
+  return prettier.format(code, options)
 })
