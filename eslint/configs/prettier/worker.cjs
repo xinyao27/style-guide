8f89e742a8c57e35a152b1fb3d7840a3940c6382
Xinyao <hi@xinyao.me>
2025-04-30 13:54:55 +0800

    fix(eslint): enhance Prettier integration by resolving config based on file path

--- eslint/configs/prettier/worker.cjs
+++ eslint/configs/prettier/worker.cjs
@@ -6,5 +6,9 @@ let prettier
 
 runAsWorker(async (code, options) => {
   if (!prettier) prettier = await import('prettier')
+  if (options.filepath) {
+    const config = await prettier.resolveConfig(options.filepath)
+    options = { ...config, ...options }
+  }
   return prettier.format(code, options)
 })
